613e1e24ba6d75a0d682d1d963cd6eeb92adec43
Aaron Pan <pan.aa@northeastern.edu>
2026-04-29 18:18:20 -0400

    Dockerfile for testing jetson setup scripts

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-docker build -f "$REPO_ROOT/docker/Dockerfile_jetson" -t stereo-pipeline:latest "$REPO_ROOT"
+REPO_NAME="$(basename "$REPO_ROOT")"
+docker build \
+  --build-arg "REPO_NAME=$REPO_NAME" \
+  -f "$REPO_ROOT/docker/Dockerfile_jetson" \
+  --network=host \
+  -t stereo-pipeline:latest \
+  "$REPO_ROOT"
