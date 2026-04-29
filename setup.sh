2e8acd3dfb1b73dbf140b0051f056046dbee0b49
Aaron Pan <pan.aa@northeastern.edu>
2026-04-29 18:17:58 -0400

    add pydbow3, requirements.txt, opencv for jetpack, v4l2 camera, and torch for jetson in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 set -e
 
 # pull submodules
-git submodule update --init --recursive
+# git submodule update --init --recursive
 
 # update apt repo
 sudo apt-get update
 
 # pull lfs
-sudo apt-get install git-lfs
-git lfs install
-git lfs pull
+# sudo apt-get install git-lfs
+# git lfs install
+# git lfs pull
 
 # install pycuvslam stuff
 cd submodules/pycuvslam/
@@ -18,24 +18,43 @@ pip install -e bin/aarch64
 cd ../..
 
 # install requirements from this folder
-pip install -e .
+pip install -r requirements.txt
+# can probably remove this
+# python3 -m pip install --upgrade "packaging==24.2" "setuptools==79.0.1"
 
 # install opencv and cv_bridge from source
+# sudo apt install -y python3-colcon-common-extensions
 cd scripts
-./install_opencv4.10.0_Jetpack6.1.sh
+printf 'yes\n' | ./install_opencv4.10.0_Jetpack6.1.sh
 ./install_cv_bridge.sh
+cd ..
 
 # install unzip tools
-sudo apt-get install unzip
+# sudo apt-get install unzip
 
-# install pydbow3
+# unzip pydbow3
 cd external
 if [ ! -d "PyDBoW3" ]; then
     sudo unzip PyDBoW3.zip
 fi
-cd PyDBoW3
+
+# Build and install native DBoW3 dependency first.
+cd PyDBoW3/modules/dbow3
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local
+cmake --build build -j"$(nproc)"
+sudo cmake --install build
+export DBoW3_DIR="/usr/local/lib/cmake/DBoW3"
+
+# install pydbow3
+cd ../..
 pip install -e .
 
 # return to home directory
 cd ../..
 
+cd scripts
+./install_v4l2_camera.sh
+
+./install_torch.sh
